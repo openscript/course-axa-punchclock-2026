bdd3d966d95bccfe55d482f3513ce472778801e3
Robin <git+r@obin.ch>
2026-06-25 09:05:10 +0000

    chore: configure openapi

--- src/main/java/ch/axa/punchclock/PunchclockApplication.java
+++ src/main/java/ch/axa/punchclock/PunchclockApplication.java
@@ -3,7 +3,10 @@ package ch.axa.punchclock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 @SpringBootApplication
+@OpenAPIDefinition
 public class PunchclockApplication {
 
 	public static void main(String[] args) {
